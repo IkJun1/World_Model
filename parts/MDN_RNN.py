a185d8b3e1a83d865bd7bd078d9ea8ce66341b5c
IkJun <wasd323@naver.com>
2025-08-05 01:10:10 +0000

    update parts

--- parts/MDN_RNN.py
+++ parts/MDN_RNN.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch import distributions
+from torch.nn import functional as F
 from torch.utils.data import Dataset
 import numpy as np
 
@@ -11,7 +12,7 @@ class MDN(nn.Module):
         self.num_dist = num_dist
         self.latent_size = latent_size
         self.tau = temperature
-
+        
         self.phi_linear = nn.Linear(input_size, num_dist)
         self.mu_linear = nn.Linear(input_size, num_dist*latent_size)
         self.sigma_linear = nn.Linear(input_size, num_dist*latent_size)
@@ -41,26 +42,34 @@ class MDN_RNN(nn.Module):
 
         self.lstm = nn.LSTM(input_size=input_size+action_size, hidden_size=hidden_size, num_layers=3, batch_first=True)
         self.mdn = MDN(input_size=hidden_size, num_dist=num_dist, latent_size=latent_space_size, temperature=temperature)
+        self.reward_linear = nn.Linear(hidden_size, 1)
 
     def forward(self, input_z_vector, a_t_onehot):
         vector_sequence = torch.cat([input_z_vector, a_t_onehot], dim=2)
         output, _ = self.lstm(vector_sequence)
         B, Seq, hidden_size = output.size()
-        output = output.view(-1, hidden_size) # predict at hidden states of all sequences
+        output = output.reshape(-1, hidden_size) # predict at hidden states of all sequences
 
         mu, sigma, phi = self.mdn(output)
         mu, sigma, phi = mu.view(B, Seq, self.num_dist, -1), sigma.view(B, Seq, self.num_dist, -1), phi.view(B, Seq, -1)
 
-        return mu, sigma, phi
+        reward = self.reward_linear(output)
+        reward = reward.view(B, Seq)
+
+        return mu, sigma, phi, reward, output
     
 def sampling(mu, sigma, phi):
     mixture_distribution = distributions.Categorical(probs=phi)
     component_distribution = distributions.Normal(loc=mu, scale=sigma)
-    mixture_gaussian = distributions.MixtureSameFamily(mixture_distribution, component_distribution)
+    component_dist = distributions.Independent(
+        component_distribution,
+        reinterpreted_batch_ndims=1
+    )
+    mixture_gaussian = distributions.MixtureSameFamily(mixture_distribution, component_dist)
 
     return mixture_gaussian.sample()
 
-def mdn_rnn_loss(mu, sigma, phi, target):
+def mdn_rnn_loss(mu, sigma, phi, target, p_reward, t_reward, reward_weights=1): # p_reward is predicted reward by the model, t_reward is target reward
     dist = distributions.Normal(loc=mu, scale=sigma)
     target = target.unsqueeze(1)
     log_prob = dist.log_prob(target)
@@ -70,12 +79,16 @@ def mdn_rnn_loss(mu, sigma, phi, target):
     weighted_log_prob = log_phi + joint_log_prob # originally prob * phi, but log_prob + log_phi since we're in log-space
     log_likelihood = torch.logsumexp(weighted_log_prob, dim=-1)
 
-    return -log_likelihood.mean()
+    mse = F.mse_loss(p_reward, t_reward)
+
+    return -log_likelihood.mean() + reward_weights*mse
 
 class SequenceDataset(Dataset):
-    def __init__(self, image_dataset, transforms, action_dataset, sequence_length=1000):
+    def __init__(self, image_dataset, transforms, action_dataset, reward_dataset, sequence_length=1000):
         self.images = image_dataset
         self.actions = action_dataset
+        self.rewards = reward_dataset
+
         self.length = sequence_length
         self.transforms = transforms
 
@@ -87,5 +100,6 @@ class SequenceDataset(Dataset):
         images_transform = [self.transforms(image) for image in images]
         images_tensor = torch.stack(images_transform)
         actions = self.actions[idx: idx+self.length]
+        rewards = self.rewards[idx: idx+self.length]
 
-        return images_tensor, torch.tensor(actions, dtype=torch.float32)
+        return images_tensor, torch.tensor(actions, dtype=torch.float32), torch.tensor(rewards, dtype=torch.float32)
